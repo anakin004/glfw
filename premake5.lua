8b655c8a8e08e804d6d0956a1bebc6e127f466c7
Ryan Mangeno <ryanmangeno@gmail.com>
2024-12-26 05:01:37 -0500

    premake update

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,8 @@
 project "GLFW"
     kind "StaticLib"
     language "C"
-
+	staticruntime "on"
+			
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
@@ -32,7 +33,6 @@ project "GLFW"
     
     filter "system:windows"
         systemversion "latest"
-        staticruntime "On"
 
 
         files
@@ -57,5 +57,10 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
     
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+    filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
